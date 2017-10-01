c8f0832c0ee56e884001889932cd4e90be317e77
b0rtvader <bort.dm@gmail.com>
2017-10-01 22:43:59 +0500

    Initial commit

---
+++ opencv-python.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while 1:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    hand_cascade = cv2.CascadeClassifier('haarcascade_hand.xml')
+
+    hands = hand_cascade.detectMultiScale(img, 1.3, 5)
+
+    for (x,y,w,h) in hands:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+
+
+    cv2.imshow('img',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
